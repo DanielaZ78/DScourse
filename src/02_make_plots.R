3b1c1736930a316cfa35d625422b16f4bfff800a
DanielaZ78 <d.salopek@gmail.com>
2024-05-21 17:59:01 +0000

    improved plot

--- src/02_make_plots.R
+++ src/02_make_plots.R
@@ -8,5 +8,26 @@ ggplot() +
 
 ggsave(filename = "figs/lifeExp_vs_gdpPercap_2007.pdf")
 
+d|> 
+  dplyr::filter(year == 1952) |>
+  ggplot() + 
+  geom_point(aes(x = gdpPercap, y = lifeExp)) 
 
+d|> 
+  dplyr::filter(year == 1952)
+  
+outlier_country <- d
+  arrange(desc(gdpPercap)) |>
+    slice(1)
+  
+print(outlier_country)
 
+d|> 
+  dplyr::filter(year == 2007) |>
+  ggplot() + 
+  geom_point(aes(x = gdpPercap, y = lifeExp, size = pop, colour = continent)) + scale_x_log10()
+
+d|> 
+  dplyr::filter(year == 1952) |>
+  ggplot() + 
+  geom_point(aes(x = gdpPercap, y = lifeExp, size = pop, colour = continent)) 
